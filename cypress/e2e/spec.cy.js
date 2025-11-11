55a1aff92cd0c8dc69719e9003706ad2af43f2c4
Bruno Carvalho <brunobzs@gmail.com>
2025-11-11 08:53:08 -0300

    First commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,29 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('')
+    cy.get('.site-branding__name').should('be.visible')
+  })
+
+
+  it('Should display admin page', () => {
+    cy.visit('/user')
+    cy.get('[data-drupal-selector="user-login-form"]').should('be.visible')
+  })
+
+  it('Should login as admin', () => {
+    cy.login()
+  })
+
+    it('[Responsiviness] Menu should be opened', () => {
+    cy.viewport('iphone-6')
+    cy.get('[data-drupal-selector="mobile-nav-button"]').click()
+    cy.get('[data-drupal-selector="header-nav"]').should('be.visible')
+  })
+
+  it('Should logout', () => {
+    cy.login()
+    
+    cy.visit('/user/logout')
+    cy.contains('input', 'Log out').click()
+  })
+});
